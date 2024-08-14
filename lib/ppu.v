cd94cf43ecbb0a8b5c2c6ebbda4b6f5f515b4c2e
Arthur Zhang <azhangvo@gmail.com>
2024-08-13 22:47:09 -0400

    some ppu logic

--- lib/ppu.v
+++ lib/ppu.v
@@ -443,7 +443,7 @@ module PixelMuxer(input [3:0] bg, input [3:0] obj, input obj_prio, output [3:0] 
   wire bg_flag = bg[0] | bg[1];
   wire obj_flag = obj[0] | obj[1];
   assign is_obj = !(obj_prio && bg_flag) && obj_flag;
-  assign out = is_obj ? obj : bg;
+  assign out = is_obj ? 4'b0001 : bg;
 endmodule
  
 
--- ppu/ppu.sv
+++ ppu/ppu.sv
@@ -19,11 +19,37 @@ module PPU(
         output [19:0] mapper_ppu_flags
     );
     
-    reg [7:0] ppuctrl;
-    reg is_even;
+    reg [7:0] PPUCTRL;
+    // Bit 1 & 0 - 00 = 2000, 01 = 2400, 10 = 2800, 11 = 2C00
+    // Bit construct 0010bb00xxxxxxxx
+    // Bit construct 0010bb00hhhhwwww
+    reg [7:0] PPUMASK;
+    reg [7:0] PPUSTATUS;
+    reg [7:0] OAMADDR;
+    reg [7:0] OAMDATA;
+    reg [7:0] PPUSCROLL;
+    reg [7:0] PPUADDR;
+    reg [7:0] PPUDATA;
 
-    wire is_end_of_scanline;
-    wire is_end_of_frame;
+    reg [7:0] cam_position_x;
+    reg [7:0] cam_position_y;
+    reg [7:0] cam_offset_x;
+    reg [7:0] cam_offset_y;
+
+    reg is_even; // Keep track of if the current frame is even or not
+
+    wire is_end_of_scanline; // End of scanline usually occurs on cycle 341, but happens on cycle 340 for special idle scanline 261 (or -1) if the frame is odd
+    wire is_end_of_frame; // 260 is the last scanline, end of frame happens when we are on the last scanline and at its end.
+    wire is_in_vblank;
+    wire is_entering_vblank; // We should enter vblank on scanline 240
+    wire is_exiting_vblank; // We should exit vblank after scanline 260, or on scanline 261 (-1)
+    wire is_signaling_cpu; // We should signal the CPU with vblank flag and nmi on tick 1 (second tick) of scanline 240. 
+    wire is_in_background_stage;
+    wire [2:0] background_stage; // represent what stage in background fetch we are in
+                                 // 001/010 - nametable byte
+                                 // 011/100 - attribute table byte
+                                 // 101/110 - pattern table tile low
+                                 // 111/000 - pattern table tile high
 
     assign is_end_of_scanline = (!is_even && scanline == 9'b111111111) ? cycle == 340 : cycle == 341;
     assign is_end_of_frame = is_end_of_scanline && scanline == 260;
@@ -37,10 +63,36 @@ module PPU(
         else if(ce) begin
             scanline <= !is_end_of_scanline ? scanline      :
                         is_end_of_frame     ? -1            :
-                                              scanline + 1;
+                                              scanline + 1  ;
+
             cycle <= is_end_of_scanline ? 0 : cycle + 1;
             is_even <= is_end_of_frame ? !is_even : is_even;
+
+            // $display("%d %d %d", cycle, is_in_background_stage, background_stage);
         end
+    end
+                                 
+    always @ (posedge clk) begin
+
+    end
+
+    assign is_in_background_stage = cycle >= 1 && cycle <= 256;
+    wire [8:0] background_stage_cycle;
+    assign background_stage_cycle = cycle - 1;
+    assign background_stage = background_stage_cycle[2:0];
+    wire [4:0] next_cam_offset_x;
+    wire [4:0] next_cam_offset_y;
+    assign next_cam_offset_x = background_stage_cycle[7:3] + 5'b10;
+    assign next_cam_offset_y = scanline[7:3];
+
+    // We want to start loading tile 3 on tick 1
+    // Bit construct 0010bb00hhhhwwww
+    assign vram_a = 
+            background_stage[2:1] == 0 ? {2'b10, PPUCTRL[1:0], next_cam_offset_y, next_cam_offset_x} :
+            14'b0;
+
+    always @ (posedge clk) begin
+
     end
 
 endmodule
